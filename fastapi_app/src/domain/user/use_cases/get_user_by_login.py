162725a2f8224e22f5ec90f9c48aa749fe5049c4
CuasarN1 <55405975+CuasarN1@users.noreply.github.com>
2026-03-23 09:41:59 +0400

    Added error logging

--- fastapi_app/src/domain/user/use_cases/get_user_by_login.py
+++ fastapi_app/src/domain/user/use_cases/get_user_by_login.py
@@ -1,8 +1,12 @@
+import logging
+
 from infrastructure.sqlite.database import database
 from infrastructure.sqlite.repositories.users import UserRepository
 from schemas.users import User as UserSchema
 from core.exceptions.database_exceptions import UserNotFoundException
 from core.exceptions.domain_exceptions import UserNotFoundByLoginException
+
+logger = logging.getLogger(__name__)
 
 
 class GetUserByLoginUseCase:
@@ -15,6 +19,8 @@ class GetUserByLoginUseCase:
             with self._database.session() as session:
                 user = self._repo.get(session=session, login=login)
         except UserNotFoundException:
-            raise UserNotFoundByLoginException(login=login)
+            error = UserNotFoundByLoginException(login=login)
+            logger.error(error.get_detail())
+            raise error
 
         return UserSchema.model_validate(obj=user)
